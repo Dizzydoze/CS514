c83c62609efa31f1b3066a1a626ade9fd752cc61
Dizzydoze <overdosedizzy@gmail.com>
2023-10-18 20:26:33 -0700

    bug fixed

--- src/MartianGenome/GastroDiseaseDetector.java
+++ src/MartianGenome/GastroDiseaseDetector.java
@@ -8,12 +8,19 @@ package MartianGenome;
 public class GastroDiseaseDetector implements PatternDetector{
     @Override
     public int detect(GenomeAnalyzer analyzer) {
+
         String buffer = analyzer.getBuffer();
+        // for GTG, length should be >= 3
+        if (buffer.length() < 3){
+            return -1;
+        }
+        // traverse the whole buffer string, find GT*G patterns
         for (int i = 0; i < buffer.length() - 2; i++) {
             if (buffer.charAt(i) == 'G' && buffer.charAt(i + 1) == 'T'){
                 // loop the rest of the buffer
-                for (int j = i + 1; j < buffer.length() - 1; j++) {
+                for (int j = i + 2; j < buffer.length(); j++) {
                    if (buffer.charAt(j) != 'T' && buffer.charAt(j) != 'G'){
+                       i = j + 1;
                        break;
                    }
                    else if(buffer.charAt(j) == 'G'){
